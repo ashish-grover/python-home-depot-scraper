156519ec46acc111ac24ab88ebe94dd22699a118
Ashish Grover <agrover@upstreamworks.com>
2020-06-07 20:53:16 -0400

    This commit changes the message to microsoft teams instead of an sms number.

--- homedepot.py
+++ homedepot.py
@@ -1,28 +1,24 @@
 import sys
 import os
 import requests
-from requests.auth import HTTPBasicAuth
 import json
 
-from twilio.rest import Client
+import pymsteams
 
-if len(sys.argv) != 4:
-    print('Please provide the product number and how many stores to search.')
-    print('for example: homedepot.py 1000155721 10 +15555555555')
+if len(sys.argv) != 3:
+    print('Please provide the product number(s) and how many stores to search.')
+    print('for example: homedepot.py 1001470384,1001533118 10')
     sys.exit(1)
 
 ##set up home depot variables
-product_number = sys.argv[1]
+product_numbers = [str(i) for i in (sys.argv[1]).split(',')]
 product_name = ''
 total_stores_to_search = sys.argv[2]
 
 ##set up twilio variables
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-client = Client(account_sid, auth_token)
+msteams_connector_url = os.environ['MICROSOFT_TEAMS_CONNECTOR_URL']
 message_body = ''
 send_message = False
-send_to_phone_number = sys.argv[3]
 
 headers = {
         'Accept':'application/json'
@@ -39,52 +35,54 @@ response_stores = requests.request(
         headers = headers
     )
 
- ##this is nuts - a separate api call has to be done to get the product name...
-url_product = 'https://www.homedepot.ca/homedepotcacommercewebservices/v2/homedepotca/products/' + product_number + '.json?fields=BASIC_SPA&lang=en'
-response_product = requests.request(
-        'GET',
-        url_product,
-        headers = headers
-    )
+for product_number in product_numbers:
+    ##this is nuts - a separate api call has to be done to get the product name...
+    url_product = 'https://www.homedepot.ca/homedepotcacommercewebservices/v2/homedepotca/products/' + product_number + '.json?fields=BASIC_SPA&lang=en'
+    response_product = requests.request(
+            'GET',
+            url_product,
+            headers = headers
+        )
 
-if response_product.status_code == 200:
-    data = json.loads(response_product.text)
-    product_name = json.loads(response_product.text)['manufacturer']+ ' - ' + json.loads(response_product.text)['name']
+    if response_product.status_code == 200:
+        data = json.loads(response_product.text)
+        product_name = json.loads(response_product.text)['manufacturer']+ ' - ' + json.loads(response_product.text)['name']
 
-message_header = 'Product being searched for: ' + product_name + '\n\n'
-print(message_header)
+    message_header = 'Product being searched for: ' + product_name + '\n\n'
+    message_header += '=================================================== \n\n'
+    print(message_header)
 
-if response_stores.status_code == 200:
-    stores = json.loads(response_stores.text)['stores']
-        
-    if stores:
-        for store in stores:
-            url = 'https://www.homedepot.ca/homedepotcacommercewebservices/v2/homedepotca/products/' + product_number + '/localized/' + store['name'] + '?fields=BASIC_SPA&lang=en'
-            response = requests.request(
-                    'GET',
-                    url,
-                    headers = headers
-                )
+    if response_stores.status_code == 200:
+        stores = json.loads(response_stores.text)['stores']
             
-            if response.status_code == 200:
-                send_message = False
-                stock_level = json.loads(response.text)['storeStock']['stockLevel']
-                if (stock_level > 1):
-                    output = 'There are ' + str(stock_level) + ' in stock at store ' + store['name'] + ', located at ' + store['address']['formattedAddress']
-                    print(output)
-                    message_body += output + '\n'
-                    send_message = True
-                elif (stock_level == 1):
-                    output = 'There is ' + str(stock_level) + ' in stock at store ' + store['name'] + ', located at ' + store['address']['formattedAddress']
-                    print(output)
-                    message_body += output + '\n'
-                    send_message = True                
+        if stores:
+            message_body = ''
+            send_message = False
+            for store in stores:
+                url = 'https://www.homedepot.ca/homedepotcacommercewebservices/v2/homedepotca/products/' + product_number + '/localized/' + store['name'] + '?fields=BASIC_SPA&lang=en'
+                response = requests.request(
+                        'GET',
+                        url,
+                        headers = headers
+                    )
+                
+                if response.status_code == 200:                    
+                    stock_level = json.loads(response.text)['storeStock']['stockLevel']
+                    if (stock_level > 1):
+                        output = 'There are ' + str(stock_level) + ' in stock at store ' + store['name'] + ', located at ' + store['address']['formattedAddress']
+                        print(output)
+                        message_body += output + '\n\n'
+                        send_message = True
+                    elif (stock_level == 1):
+                        output = 'There is ' + str(stock_level) + ' in stock at store ' + store['name'] + ', located at ' + store['address']['formattedAddress']
+                        print(output)
+                        message_body += output + '\n\n'
+                        send_message = True
 
-if send_message:
-    message = client.messages.create(
-                            body = message_header + message_body,
-                            from_ = '+16477230044',
-                            to = send_to_phone_number
-                        )
-else:
-    print('No stock available at any of the closest ' + total_stores_to_search + ' stores')
+    if send_message:
+        ##send messages to Microsoft Teams
+        myTeamsMessage = pymsteams.connectorcard(msteams_connector_url)
+        myTeamsMessage.text(message_header + message_body)
+        myTeamsMessage.send()        
+    else:
+        print('No stock available at any of the closest ' + total_stores_to_search + ' stores\n')
